4aa9c4a62fd0f6b6d2cee072b0b4edcc2a5c42e9
Jorge Andrés Beltrán Gargallo <gsusuario20222023@gmail.com>
2025-06-26 20:46:21 +0200

    mi primer commit

---
+++ mainn.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class mainn {
+
+    public static void main(String[] args) {
+
+        // scanner para
+        Scanner scanner = new Scanner(System.in);
+
+
+        git remote add origin https://github.com/jorgeandres1818/miRepositorio.git
+    }
+    
+}
