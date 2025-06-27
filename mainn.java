1d60eb7ea798c15cdc579e436119adf5a185de13
Jorge Andrés Beltrán Gargallo <gsusuario20222023@gmail.com>
2025-06-27 19:15:06 +0200

    cambio

--- mainn.java
+++ mainn.java
@@ -1,16 +1,23 @@
 import java.io.*;
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
 
 public class mainn {
 
     public static void main(String[] args) {
 
-        // scanner para
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nombres = new ArrayList<>();
 
-        System.out.println("Hola desde github!!");
-        System.out.println("Probando otra cosa");
-        
+        // Agregar elementos
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Marta");
+        nombres.add("Pedro");
+
+        // Mostrar todos los nombres
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
     }
     
 }
